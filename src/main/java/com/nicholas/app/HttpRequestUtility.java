b2b093cbaf9b6823c6fea1e75a7c11855f34c3ce
SingularSquids <nicholas.lee.h@icloud.com>
2025-08-11 16:03:22 -0400

    added HttpRequestUtility file
    Added a utility class to manage sending and receiveing http requests and responses, and updated the save button actionlistener to call the appropriate utility class method.

---
+++ src/main/java/com/nicholas/app/HttpRequestUtility.java
@@ -0,0 +1,47 @@
+package com.nicholas.app; 
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.lang.reflect.Type;
+import com.google.gson.reflect.TypeToken;
+import java.net.URL;
+import java.net.HttpURLConnection;
+import java.io.OutputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.BufferedReader; 
+import com.google.gson.Gson;
+public class HttpRequestUtility{
+    private static Gson gson = new Gson();
+
+    public static <T> void HttpPutRequest(String stringUrl,String token,T requestBody){
+        try{
+            URL url = new URL(stringUrl); 
+            var conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("PUT");
+            conn.setRequestMethod("PUT");
+            conn.setRequestProperty("Authorization", "Bearer " + token);
+            conn.setRequestProperty("Content-Type","application/json; utf-8");
+            conn.setDoOutput(true);           
+            String json = gson.toJson(requestBody);
+            try(OutputStream os = conn.getOutputStream()){
+                os.write(json.getBytes());
+            }
+            int responseCode = conn.getResponseCode();
+            InputStream is = responseCode == 200 ? conn.getInputStream():conn.getErrorStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+            String responseBody = reader.lines().collect(Collectors.joining("/n"));
+            reader.close();
+            
+            if (responseCode == 200){
+                System.out.println(responseBody);
+            } else {
+                Type type = new TypeToken<Map<String,String>>(){}.getClass();
+                Map<String,String> errorMap = gson.fromJson(responseBody,type);
+                System.out.println(errorMap.get("error"));
+            }
+            conn.disconnect();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
